69199f96f218d38f8703290cfc3daa5392da0abe
hehe <hehe@qq.com>
2019-08-20 16:51:51 +0800

    修改本地工程

--- git-test-project/src/com/wahaha/git/demo.java
+++ git-test-project/src/com/wahaha/git/demo.java
@@ -2,8 +2,14 @@ package com.wahaha.git;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("我真帅...");
-        System.out.println("我真帅...");
-        System.out.println("我真帅...");
+        System.out.println("哇塞...");
+        System.out.println("哇塞...");
+        System.out.println("哇塞...");
+
+    }
+    public void test(){
+        for (int i = 0; i <5; i++) {
+            System.out.println("撒拉嘿呦");
+        }
     }
 }
